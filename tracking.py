5984d6ddf47b1e26fdd1795886213640a571893b
mrLevin778 <vitaliy.respect@gmail.com>
2023-04-11 00:31:09 +0300

    Added more statuses for goods and minor fixes

--- tracking.py
+++ tracking.py
@@ -2,7 +2,6 @@ import requests
 import json
 
 url = 'https://api.novaposhta.ua/v2.0/json/'
-trackID = '20400318832057'  # these goods are delivered
 
 
 # apiKey - your personal api key from NP, but we can get status code without api key
@@ -13,28 +12,46 @@ trackID = '20400318832057'  # these goods are delivered
 class TrackingNP:
     """Main class"""
 
-    def __init__(self, trackID):
+    def __init__(self, trackID, phone):
         self.trackID = trackID
+        self.phone = phone
 
-    @staticmethod
-    def statusCheck():
+
+    def status_check(self):
         """Send request for NP with our track number and get status"""
-        param = {"apiKey": "", "modelName": "TrackingDocument", "calledMethod": "getStatusDocuments",
-                 "methodProperties": {"Documents": [{"DocumentNumber": str(trackID), "Phone": ""}]}}
+        param = {
+            "apiKey": '',
+            "modelName": "TrackingDocument",
+            "calledMethod": "getStatusDocuments",
+                 "methodProperties": {
+                     "Documents": [{"DocumentNumber": str(self.trackID), "Phone": str(self.phone)}]
+                 }
+        }
         json_param = json.dumps(param)
         resp = requests.get(url, data=json_param)
         r = resp.json()  # parse json
-        data_list = r["data"]
+        data_list = r['data']
         data_dict = data_list[0]
-        status_code = int(data_dict.get("StatusCode"))
-        if status_code == 11 or status_code == 9 or status_code == 10 or status_code == 106:
+        status_code = int(data_dict.get('StatusCode'))
+        if status_code == 3:
+            not_found = 'Посилку з даним номером не знайдено!'
+            return not_found
+        if status_code == 7 or 8:
+            in_warehouse = 'Посилка прибула на відділення'
+            return in_warehouse
+        if status_code == 11 or 9 or 10 or 106:
             success = 'Посилку отримано!'
             return success
+        if status_code == 4 or 41 or 5 or 6 or 101:
+            delivering = 'Посилка ще в дорозі'
+            return delivering
         else:
             not_delivered = 'Посилка ще не отримана'
             return not_delivered
 
-
-get_track = TrackingNP(trackID)
-g = get_track.statusCheck()
-print(g)
+if __name__ == '__main__':
+    trackID = '59000000000000'
+    phone = '09877777777'
+    get_track = TrackingNP(trackID, phone)
+    g = get_track.status_check()
+    print(g)
